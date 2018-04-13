d614c75d6238802816467708d7342f993da2fc84
Vedica <vedicavraje@gmail.com>
2018-04-13 13:39:26 +0530

    initial commit

---
+++ app/src/main/java/com/example/vedic/colorgameforgacs/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.vedic.colorgameforgacs;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+Button but1=(Button)findViewById(R.id.but1);
+    Button but2=(Button)findViewById(R.id.but2);
+    Button but3=(Button)findViewById(R.id.but3);
+    Button but4=(Button)findViewById(R.id.but4);
+    Button but5=(Button)findViewById(R.id.but5);
+    Button but6=(Button)findViewById(R.id.but6);
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+       // but1.setBackgroundColor(Color.rgb(100,100,100));
+    }
+
+}
